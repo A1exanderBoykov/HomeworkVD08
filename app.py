aa60b72c0f02626f586909b65880412e4db0429b
A1exanderBoykov <pups1k89@mail.ru>
2024-07-29 13:10:57 +0300

    add code

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = requests.get('https://api.quotable.io/random')
+    if response.status_code == 200:
+        data = response.json()
+        quote = data['content']
+        author = data['author']
+    else:
+        quote = "Не удалось получить цитату. Попробуйте позже."
+        author = ""
+
+    return render_template('index.html', quote=quote, author=author)
+
+if __name__ == '__main__':
+    app.run(debug=True)
